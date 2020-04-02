3f6e80f7fcbae079705cae8a52f0b392b80aa7ec
Павел <taxa3008@gmail.com>
2020-04-02 21:01:22 +0300

    Рабочий вариант

--- test_items.py
+++ test_items.py
@@ -1,4 +1,4 @@
-def test_go_to_es_site(browser):
+def test_add_to_cart_button(browser):
     link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
     browser.get(link)
     assert browser.find_element_by_xpath('//article//form/button')
